075daec7fdcff00bb4eae2942629f5cd982f9823
dbrehm <brehm.derek@gmail.com>
2020-03-27 12:46:36 -0400

    add debugging for trigger lookup.

--- Framework/AnalysisModules/B2G-20-004/triggerlookup.cc
+++ Framework/AnalysisModules/B2G-20-004/triggerlookup.cc
@@ -12,8 +12,10 @@ namespace analyzer {
         std::vector<float> out;
 
         if (var < 4000.0){
-            int bin0 = TRP->GetBin(var); 
+            int bin0 = TRP->GetBin(var);
+            cout << "Got bin." << endl;
             float jetTriggerWeight = TRP->GetEfficiency(bin0);
+            cout << "Got weight." << endl;
             // Check that we're not in an empty bin in the fully efficient region
             if (jetTriggerWeight == 0){
                 if ((TRP->GetEfficiency(bin0-1) == 1.0) && (TRP->GetEfficiency(bin0+1) == 1.0)){
@@ -22,6 +24,7 @@ namespace analyzer {
                     jetTriggerWeight = (TRP->GetEfficiency(bin0-1)+TRP->GetEfficiency(bin0+1))/2.0;
                 }
             }
+            cout << "Checked for fully efficient region." << endl;
 
             Weight = jetTriggerWeight;
             float deltaTriggerEff  = 0.5*(1.0-jetTriggerWeight);
@@ -29,6 +32,7 @@ namespace analyzer {
             float zero = 0.0;
             Weightup = std::min(one,(jetTriggerWeight + sqrt(pow((deltaTriggerEff),2) + pow(TRP->GetEfficiencyErrorUp(bin0),2) ))) ;
             Weightdown = std::max(zero,(jetTriggerWeight - sqrt(pow((deltaTriggerEff),2) + pow(TRP->GetEfficiencyErrorLow(bin0),2) )));
+            cout << "Weights made." << endl;
 
         }    
         out.push_back(Weight);
