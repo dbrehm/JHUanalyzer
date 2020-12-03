ce47168664ebc452d0a89433dac98f15035eed57
dbrehm <brehm.derek@gmail.com>
2020-12-03 14:30:53 -0500

    another fix

--- Framework/AnalysisModules/B2G-20-004/hhAll_preselection_graph.py
+++ Framework/AnalysisModules/B2G-20-004/hhAll_preselection_graph.py
@@ -737,7 +737,7 @@ if a.isData and options.year == "18":
     if "D" in setname or "C" in setname:
         slim_skim.Add("HEM15_16_2018dataCut","(!(-2.5 < "+eta0+" && "+eta0+" < -1.3) && !(-2.5 < "+eta1+" && "+eta1+" < -1.3)")
     if "B" in setname:
-        slim_skim.Add("HEM15_16_2018dataCut","((run < 319077) && (!(-2.5 < "+eta0+" && "+eta0+" < -1.3) && !(-2.5 < "+eta1+" && "+eta1+" < -1.3))")
+        slim_skim.Add("HEM15_16_2018dataCut","((run < 319077) && (!(-2.5 < "+eta0+" && "+eta0+" < -1.3) && !(-2.5 < "+eta1+" && "+eta1+" < -1.3)))")
 
 filters = CutGroup('filters')
 filters.Add("Flag_goodVertices","Flag_goodVertices == 1")
